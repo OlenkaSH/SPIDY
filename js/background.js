ef0180283ff62e7516c6cb1d64a3a7fc59fdd812
Olena Kraemer <olenakraem@gmail.com>
2021-11-03 14:38:43 +0100

    the fly is moving

--- js/background.js
+++
@@ -1,8 +0,0 @@
-class Background {
-    draw() {
-        console.log('this is the background drawing')
-        Image(game.backgroundImages[0].src, 0, 0, width, height)
-    }
-
-    
-}
--- main.js
+++ main.js
@@ -2,29 +2,41 @@ let playerImage;
 let playerX = 100;
 let flyImage;
 let fly = 200;
+let speed = 3;
+let backgroundImage;
+let spidernet1 = 150;
+
+
 
 function preload() {
+  
   playerImage = loadImage('spidy.png')
   flyImage = loadImage('fly.png')
+  backgroundImage = loadImage('2.png')
 
 }
 
 function setup() {
 
   console.log('setup')
-  createCanvas(600, 400);
+  createCanvas(1000, 800);
   
 
 }
-
+//let fly = 50
+//let speed = 3
 
 function draw() {
-  //clear()
+  clear()
   console.log('draw')
 
   //render the player image (x,y,width,height)
 
   image(playerImage, playerX, 20, 0, 0)
   image(flyImage, fly, 90, 0, 0)
+  image(backgroundImage, spidernet1, 50, 450, 350)
 
+
+  fly += speed
 }
+
