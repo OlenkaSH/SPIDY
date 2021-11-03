d6e03d7c475b622f51f9165115a5e2f4cbf12d9e
Olena Kraemer <olenakraem@gmail.com>
2021-11-03 15:19:36 +0100

    spidy is moving too

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let playerImage;
 let playerX = 100;
 let flyImage;
-let fly = 200;
+let fly = 50;
 let speed = 3;
 let backgroundImage;
 let spidernet1 = 150;
@@ -16,21 +16,18 @@ function preload() {
 
 }
 
+
 function setup() {
 
   console.log('setup')
   createCanvas(1000, 800);
   
-
 }
-//let fly = 50
-//let speed = 3
+
 
 function draw() {
   clear()
   console.log('draw')
-
-  //render the player image (x,y,width,height)
 
   image(playerImage, playerX, 20, 0, 0)
   image(flyImage, fly, 90, 0, 0)
@@ -38,5 +35,31 @@ function draw() {
 
 
   fly += speed
+  
+  if (fly > 940) {
+    //change fly's direction
+    speed = - 3
+  } else if (fly < 10) {
+    speed = + 3
+  }
+
+
+}
+
+
+function keyPressed() {
+  if (keyCode === 39) {
+    playerX += 30;
+  }
+  if (keyCode === 37) {
+    playerX -= 30;
+  }
+
+
+
+
+
+
+
 }
 
